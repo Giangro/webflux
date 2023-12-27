104630f3fd15ffb095ae4aa8f7f219ad924ecfba
Alex <alessandro.giangreco@gmail.com>
2023-12-27 02:58:49 +0100

    Windowing and Delay

--- src/main/java/com/example/webflux/handler/HelloWorldHandler.java
+++ src/main/java/com/example/webflux/handler/HelloWorldHandler.java
@@ -1,14 +1,17 @@
 package com.example.webflux.handler;
 
 import java.time.Duration;
+import java.util.concurrent.Executors;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @Component
 public class HelloWorldHandler {
@@ -21,12 +24,14 @@ public class HelloWorldHandler {
 	public Mono<ServerResponse> helloWorldStream(ServerRequest request) {
 		return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
 				.body(BodyInserters.fromPublisher(
-						Flux.range(1, 100)
-								.map(i -> {
-									return String.format("#%d Hello World from Webflux Demo v0.0.2\n", i);
-								})
-								.delayElements(Duration.ofSeconds(1)),
-						String.class));
+						Flux.range(1, 100000)						
+							.window(1000)
+							.delayElements(Duration.ofSeconds(30))
+							.flatMap(it->it.parallel())
+							.map(i -> {
+								return String.format("#%d-%s Hello World from Webflux Demo v0.0.2\n",i,Thread.currentThread().getName() );
+							})							
+						,String.class));
 	}
 
 }
