9675241e3b8c33d674a26e48028843a3b8440686
Alex <alessandro.giangreco@gmail.com>
2024-01-01 16:14:04 +0100

    call rest via webclient

--- src/main/java/com/example/webflux/handler/HelloWorldHandler.java
+++ src/main/java/com/example/webflux/handler/HelloWorldHandler.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -14,6 +15,8 @@ import reactor.core.scheduler.Schedulers;
 @Component
 public class HelloWorldHandler {
 
+
+
 	public Mono<ServerResponse> helloWorld(ServerRequest request) {
 		return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
 				.body(BodyInserters.fromValue("Hello World from Webflux Demo v0.0.3"));
@@ -22,20 +25,31 @@ public class HelloWorldHandler {
 	public Mono<ServerResponse> helloWorldStream(ServerRequest request) {
 		return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
 				.body(BodyInserters.fromPublisher(
-						Flux.range(1, 10000)
-								.window(5)
+						Flux.range(1, 1000)
+								.window(100)
 								.switchOnFirst((signal, flux) -> {									
 										return signal
 											.get()
 											.concatWith(
 												flux.skip(1)													
-													.delayElements(Duration.ofMillis(100))
+													.delayElements(Duration.ofMillis(5000))
 													.flatMap(it->it.parallel(4).runOn(Schedulers.parallel()))
 											);
-								})								
-								.map(i -> {
-									return String.format("#%d-%s Hello World from Webflux Demo v0.0.3\n", i,
-											Thread.currentThread().getName());
+								})
+								.flatMap(i -> {									
+									return Mono.zip(
+										Mono.just(i)
+										,WebClient.create("http://localhost:8080")									
+											.get()
+											.uri("/helloWorld")
+											.retrieve()
+											.bodyToMono(String.class)
+										,(a,b)->String.format("%d: %s",a,b));
+								})
+								.map(finalstr->{
+									return String.format(
+										"from %s: %s\n",Thread.currentThread().getName()
+										,finalstr);										
 								}),
 						String.class));
 	}
